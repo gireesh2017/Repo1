958cdd0f9f46180f80754470a6244c7a0108f1c6
gireesh2017 <madakagireesh@gmail.com>
2017-11-01 17:05:54 +0530

    compiled

--- Loop.java
+++ Loop.java
@@ -2,9 +2,9 @@ public class Loop
 {
 	public static void main(String args[])
 	{
-		for(i=1;i<=5;i++)
+		for(int i=1;i<=5;i++)
 		{
-			System.out.println("Iteration" +i)
+			System.out.println("Iteration" +i);
 		}
 	}
 }
